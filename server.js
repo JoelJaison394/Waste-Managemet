fd991a0c62f5f6c5b33870440ec520bd18183caa
unknown <joeljaison394@gmail.com>
2022-12-11 20:03:35 +0530

    ready for deployment

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const cookieParser = require("cookie-parser");
+const path =  require('path');
 const authRoute = require("./routes/auth")
 const toDosRoute = require("./routes/todos");
 
@@ -29,6 +30,11 @@ app.post("/name", (req,res) =>{
 
 app.use("/api/auth", authRoute)
 app.use("/api/todos", toDosRoute)
+
+app.use(express.static(path.resolve(__dirname, "./client/build")));
+app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
+  });
 
     mongoose.connect(process.env.MONGO_URI).then(()=>{
         console.log("connected to database")
