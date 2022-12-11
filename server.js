267a58538c136febbf52fe4e48fbba2ac6c0ca14
unknown <joeljaison394@gmail.com>
2022-12-11 09:49:54 +0530

    day-2

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 require("dotenv").config();
 const express = require("express");
+const mongoose = require("mongoose");
 
 const app=express();
 
@@ -23,7 +24,18 @@ app.post("/name", (req,res) =>{
 })
 
 
+    mongoose.connect(process.env.MONGO_URI).then(()=>{
+        console.log("connected to database")
+    
+        app.listen(process.env.PORT,() =>{
+            console.log(`Server running on port ${process.env.PORT}`)
+        })
+    }).catch((error)=>{
+        console.log(error);
+    });
+    
 
-app.listen(process.env.PORT,() =>{
-    console.log(`Server running on port ${process.env.PORT}`)
-})
+
+
+
+
